c2fd12856ab6e482b8a1d5b2c2a882239cbae3a4
RobertCoDuocUC <rob.cornejo@duocuc.cl>
2025-06-29 22:34:46 -0400

    Update # Menú base del programa.py
    ordene el codigo ya que lo subi mal y ahora es funcional

--- # Menú base del programa.py
+++ # Menú base del programa.py
@@ -1,16 +1,19 @@
+def datos_roberto():
+    print("Mi nombre es Roberto Cornejo y tengo 25 años.")
 # Menú base del programa
 while True:
  print("\n--- MENÚ PRINCIPAL ---")
  print("1. Función de integrante 1")
  print("2. Función de integrante 2")
+ print("3. Función de integrante 3")
  print("0. Salir")
  op = input("Seleccione opción: ")
  if op == "0":
- print("Programa finalizado.")
- break
+    print("Programa finalizado.")
+    break
  elif op == "1":
- pass # Aquí se llamará a la función del integrante 1
+    pass
  elif op == "2":
- pass # Aquí se llamará a la función del integrante 2
+    pass 
  else:
- print(" Opción inválida.")
+    print(" Opción inválida.")
